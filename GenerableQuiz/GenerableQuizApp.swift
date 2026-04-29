9909bbecb2788490a2c3eb4215bf59ef981cb906
soraya-ire <rujiangkongshi@gmail.com>
2026-04-29 16:00:20 +0900

    Initial Commit

---
+++ GenerableQuiz/GenerableQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenerableQuizApp.swift
+//  GenerableQuiz
+//
+//  Created by soraya irie on 2026/04/29.
+//
+
+import SwiftUI
+
+@main
+struct GenerableQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
