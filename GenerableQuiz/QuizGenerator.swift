7b6a3cd07b49ebd1d68f8a737f16dd17de9fb730
soraya-ire <rujiangkongshi@gmail.com>
2026-04-30 12:54:52 +0900

    Complete Section3 Step7 - generate structured content

--- GenerableQuiz/QuizGenerator.swift
+++ GenerableQuiz/QuizGenerator.swift
@@ -13,5 +13,11 @@ class QuizGenerator {
     func generateQuzi() {
         let session = LanguageModelSession(instructions: "Create a quiz with the provided topic as the focus.")
         let stream = session.streamResponse(to: topic, generating: Quiz.self)
+
+        Task {
+            for try await partial in stream {
+                self.quiz = partial.content
+            }
+        }
     }
 }
