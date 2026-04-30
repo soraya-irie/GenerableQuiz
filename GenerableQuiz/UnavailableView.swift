f2af9fc1a8950fa15014b94fe7639fbd7871178a
soraya-ire <rujiangkongshi@gmail.com>
2026-04-30 11:01:46 +0900

    Complete Section1 Step3 - generate structured content

--- GenerableQuiz/UnavailableView.swift
+++ GenerableQuiz/UnavailableView.swift
@@ -1,11 +1,14 @@
 import SwiftUI
+import FoundationModels
 
 struct UnavailableView: View {
+    let reason: SystemLanguageModel.Availability.UnavailableReason
+
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    UnavailableView()
+    UnavailableView(reason: .appleIntelligenceNotEnabled)
 }
