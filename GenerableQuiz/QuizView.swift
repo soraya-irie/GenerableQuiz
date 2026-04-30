f520d8b37fd64ea487e4f17b6221829abf953320
soraya-ire <rujiangkongshi@gmail.com>
2026-04-30 12:21:56 +0900

    Complete Section2 Step4 - generate structured content

--- GenerableQuiz/QuizView.swift
+++ GenerableQuiz/QuizView.swift
@@ -1,6 +1,8 @@
 import SwiftUI
 
 struct QuizView: View {
+    @State var quiz: Quiz
+
     private var quizStack: some View {
         Text("Quiz Stack")
     }
@@ -11,5 +13,13 @@ struct QuizView: View {
 }
 
 #Preview {
-    QuizView()
+    var sampleQuiz: Quiz {
+        Quiz(questions: [
+            "Question 1",
+            "Question 2",
+            "Question 3"
+        ])
+    }
+
+    QuizView(quiz: sampleQuiz)
 }
