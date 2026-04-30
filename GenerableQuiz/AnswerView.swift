7b9ccd0c3107aa1e592a1c843ec4b96a2666ddef
soraya-ire <rujiangkongshi@gmail.com>
2026-04-30 14:31:36 +0900

    Complete Section6 Step5 - generate structured content

--- GenerableQuiz/AnswerView.swift
+++ GenerableQuiz/AnswerView.swift
@@ -1,11 +1,14 @@
 import SwiftUI
+import FoundationModels
 
 struct AnswerView: View {
+    var displayAnswer: Answer.PartiallyGenerated
+
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    AnswerView()
+    AnswerView(displayAnswer: Answer.correctAnswer.asPartiallyGenerated())
 }
