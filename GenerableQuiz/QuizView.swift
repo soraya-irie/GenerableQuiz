120d4baff6b4a334fcd8bcc49f174e106ca71038
soraya-ire <rujiangkongshi@gmail.com>
2026-04-30 13:05:57 +0900

    Complete Section4 Step2 - generate structured content

--- GenerableQuiz/QuizView.swift
+++ GenerableQuiz/QuizView.swift
@@ -5,11 +5,13 @@ struct QuizView: View {
 
     private var quizStack: some View {
         VStack(spacing: 16) {
-            ForEach(quiz.questions, id: \.description) { question in
-                Text(question)
+            if let questions = generator.quiz?.questions {
+                ForEach(questions, id: \.description) { question in
+                    Text(question)
+                }
             }
         }
-        .navigationTitle("Sample Topic")
+        .navigationTitle(generator.topic)
         .padding()
     }
 
