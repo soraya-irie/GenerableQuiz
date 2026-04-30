9b3b821bf44a4344d6efa345f42f7c09e8b97339
soraya-ire <rujiangkongshi@gmail.com>
2026-04-30 14:46:10 +0900

    Complete Section6 Step7 - generate structured content

--- GenerableQuiz/AnswerView.swift
+++ GenerableQuiz/AnswerView.swift
@@ -3,10 +3,11 @@ import FoundationModels
 
 struct AnswerView: View {
     var displayAnswer: Answer.PartiallyGenerated
+    @Binding var selectedAnswer: Answer.PartiallyGenerated?
 
     var body: some View {
         Button {
-
+            selectedAnswer = displayAnswer
         } label: {
             Text(displayAnswer.text ?? "Generating...")
                 .multilineTextAlignment(.leading)
@@ -17,5 +18,12 @@ struct AnswerView: View {
 }
 
 #Preview {
-    AnswerView(displayAnswer: Answer.correctAnswer.asPartiallyGenerated())
+    @Previewable @State var correctAnswer = Optional(Answer.correctAnswer.asPartiallyGenerated())
+    @Previewable @State var incorrectAnswer = Optional(Answer.incorrectAnswer.asPartiallyGenerated())
+
+    VStack {
+        AnswerView(displayAnswer: correctAnswer!, selectedAnswer: $incorrectAnswer)
+        AnswerView(displayAnswer: correctAnswer!, selectedAnswer: $correctAnswer)
+        AnswerView(displayAnswer: incorrectAnswer!, selectedAnswer: $incorrectAnswer)
+    }
 }
